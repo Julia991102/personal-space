5948473770d64557d00fd9070572d5835499a468
•ꪔ̤̥ - <a-@192.168.31.50>
2026-08-05 23:43:17 +0800

    修复标签页名字

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Julia Ma | Portfolio",
-  description: "Personal Portfolio of Julia Ma",
+  title: "Sonia Ma | Portfolio",
+  description: "Personal Portfolio of Sonia Ma",
 };
 
 export default function RootLayout({
